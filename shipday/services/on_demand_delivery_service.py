4db91b88b457cc79186c1500c4983467e93191d0
shahriar <shhrrtnvr@gmail.com>
2022-08-22 18:25:59 +0600

    added availability

--- shipday/services/on_demand_delivery_service.py
+++ shipday/services/on_demand_delivery_service.py
@@ -1,12 +1,15 @@
 from shipday.utils.verifiers import verify_instance_of, verify_none_or_instance_of
 from shipday.exeptions.shipday_exeption import ShipdayException
 from shipday.httpclient.shipdayclient import ShipdayClient
+from shipday.order.address import Address
+from datetime import datetime
 
 
 class OnDemandDeliveryService:
     PATH = 'on-demand/'
     SERVICES_PATH = PATH + 'services'
     ESTIMATE_PATH = PATH + 'estimate/{order_id}'
+    AVAILABILITY_PATH = 'third-party/availability'
     ASSIGN_PATH = PATH + 'assign'
     DETAILS_PATH = PATH + 'details/{order_id}'
     CANCEL_PATH = PATH + 'cancel/{order_id}'
@@ -26,6 +29,21 @@ class OnDemandDeliveryService:
     def estimate(self, order_id) -> dict:
         verify_instance_of(int, order_id, 'Order id must be integer')
         res = self.httpclient.get(self.ESTIMATE_PATH.format_map({'order_id': order_id}))
+        return res
+
+    def check_availability(self, *args, pickup_address: Address, delivery_address: Address, delivery_time: datetime=None) -> dict:
+        verify_instance_of(Address, pickup_address, 'Pickup address must be of type {}'.format(Address))
+        verify_instance_of(Address, delivery_address, 'Delivery address must be of type {}'.format(Address))
+        verify_none_or_instance_of(datetime, delivery_time, 'Delivery Time must be of type {}'.format(datetime))
+
+        data = {
+            'pickupAddress': pickup_address.get_single_line(),
+            'deliveryAddress': delivery_address.get_single_line()
+        }
+        if delivery_time is not None:
+            data['deliveryTime'] = delivery_time.isoformat()
+
+        res = self.httpclient.post(self.AVAILABILITY_PATH, data)
         return res
 
     def assign(self, *args, order_id, service_name, tip=0, estimate_reference=None, **kwargs) -> dict:
